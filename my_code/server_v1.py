c026a66b9a20194ef2af178ee7dd024ba088f139
YI-YING1994 <01360912@me.mcu.edu.tw>
2020-10-19 10:29:26 +0800

    windows version of server is completed, and using time sleep to reduce CPU usage

--- my_code/server_v1.py
+++ my_code/server_v1.py
@@ -1,73 +1,91 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import os
 import sys
+import time
 import socket
 import threading
 
-"""
-說明： 客戶端 ip 及 port，
-      若客戶端 ip = "",
-      代表任何 ip 都可以連。
-"""
-host = ""
-port = 9999
-buffer_size = 32768 #8192
-accept_connect = 5
+host = "" # 客戶端 ip，若客戶端 ip = ""，代表任何 ip 都可以連。
+port = 9999 # 客戶端 port
+buffer_size = 32768 # 一次可傳送的資料量
+accept_connect = 5 # 允許連接的客戶端個數
+file_name = "big_file.txt" # 預設用來傳送的檔案
+program_name = os.path.basename(__file__) # 程式名稱
+program_finished = False # 程式是否結束
 
-try:
-    file_name = sys.argv[1]
-except IndexError:
-    file_name = "big_file.txt"
-
-try:
+def CheckArgv():
     """
-    說明： 創建 socket 物件，並設定 socket 
-          位址可以重複連線，如果失敗就回報錯
-          誤，並結束程式。
-          P.S. socket.AF_INET 代表使用 IPV4 Protocol,
-               socket.SOCK_STREAM 代表使用 TCP 封包。
+    說明：檢查使用者輸入在指令後的參數是否符合要求。
     """
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-except socket.error as msg:
-    print("Failed to create socket! Error code: " + str(msg.errno) + " Error message " + msg.strerror)
-    sys.exit(1)
-print("Create socket successfully!")
+    global file_name, program_name, program_finished
 
-try:
-    """
-    說明： 綁定客戶端位址，如果失敗就回報錯誤，並結束程式。
-    """
-    server.bind((host, port))
-except socket.error as msg:
-    print("Binding failed! Error code: " + str(msg.errno) + " Error message " + msg.strerror)
-    sys.exit(1)
-print("Binding socket complete!")
+    if len(sys.argv) == 1:
+        print(f"Use default file 「{file_name}」 to test download speed...")
+    elif len(sys.argv) == 2:
+        file_name = sys.argv[1]
+        print(f"Use file 「{file_name}」 to test download speed")
+    else:
+        print("Sorry, we couldn't understand what you want to do.")
+        print("Usage: ./{0} or ./{0} {1}".format(program_name, file_name))
+        program_finished = True
+        return
 
-server.listen(accept_connect) # 開始聆聽是否有客戶端請求連線，同一時間最多接受 5 個客戶端連線。
-print("Socket now listening!")
+    os.chdir(os.path.dirname(__file__))
+    if not os.path.isfile(file_name):
+        print("「{0}」 not exist, please put 「{0}」 and {1} at the same directory.".format(file_name, program_name))
+        program_finished = True
 
-"""
-說明： 與客戶端建立連線。
-connect: 用來與客戶端傳送資料的 socket 物件。
-addr: 客戶端的 ip 及 port。
-"""
-def ReceiveData(connect):
+def CreateSocket():
+    global accept_connect, program_finished
+
+    if program_finished: return
     try:
+        """
+        說明： 創建 socket 物件，並設定 socket 
+              位址可以重複連線，如果失敗就回報錯
+              誤，並結束程式。
+              P.S. socket.AF_INET 代表使用 IPV4 Protocol,
+                   socket.SOCK_STREAM 代表使用 TCP 封包。
+        """
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    except socket.error as msg:
+        print("Failed to create socket! Error code: " + str(msg.errno) + " Error message " + msg.strerror)
+        program_finished = True
+        return
+    print("Create socket successfully!")
+
+    try:
+        """
+        說明： 綁定客戶端位址，如果失敗就回報錯誤，並結束程式。
+        """
+        server.bind((host, port))
+    except socket.error as msg:
+        print("Binding failed! Error code: " + str(msg.errno) + " Error message " + msg.strerror)
+        program_finished = True
+        return
+    print("Binding socket complete!")
+
+    server.listen(accept_connect) # 開始聆聽是否有客戶端請求連線，同一時間最多接受 accept_connect 個客戶端連線。
+    print("Socket now listening!")
+
+    return server
+
+def StartConnection(connect):
+    try:
+        """
+        說明： 開啟檔案，如果成功，立即開始傳送資料，
+              失敗或傳送完畢，將與伺服器連線中的 socket 關閉。
+        """
         file_descriptor_data = open(file_name, "rb")
-        """
-        說明： 開啟檔案，如果成功，立即開始傳送資料。
-        """
         print("Start sending...")
         while True:
             data = file_descriptor_data.read(buffer_size)
             connect.sendall(data)
             if len(data) == 0:
                 break
-        """
-        說明： 傳送完畢，將伺服器與連線中的 socket 關閉。
-        """
         print("Finished\n")
     except IOError as e:
         print("\n", e)
@@ -77,16 +95,45 @@ def ReceiveData(connect):
     
     print("Waiting for new connection...")
 
-while True:
-    try:
-        connect, addr = server.accept()
-        print("\nConnected socket with " + addr[0] + ":" + str(addr[1]))
 
-        p = threading.Thread(target=ReceiveData, args=(connect,))
-        p.start()
-    except KeyboardInterrupt:
-        print("Server closed")
-        break
-server.close()   
-    
-    
+def WaitForConnection(server):
+    global program_finished
+
+    if program_finished: return
+    while not program_finished:
+        try:
+            """
+            說明： 與客戶端建立連線。
+                    connect: 用來與客戶端傳送資料的 socket 物件。
+                    addr: 客戶端的 ip 及 port。
+            """
+            connect, addr = server.accept()
+            print("\nConnected socket with " + addr[0] + ":" + str(addr[1]))
+            threading.Thread(target=StartConnection, args=(connect,), daemon=True).start()
+        except Exception as e:
+            print(e)
+    server.close()
+
+def GetUserInput():
+    global program_finished
+
+    while not program_finished:
+        user_input = input("")
+        if (user_input == "quit()"):
+            program_finished = True
+        else:
+            print("Use quit() to exit")
+
+if __name__ == "__main__":
+    CheckArgv()
+    server = CreateSocket()
+
+    threading.Thread(target=WaitForConnection, args=(server,), daemon=True).start()
+    threading.Thread(target=GetUserInput, daemon=True).start()
+
+    while True:
+        if program_finished:
+            print("Server closed...")
+            break
+        time.sleep(0.2)
+    input("Press enter to exit...")
